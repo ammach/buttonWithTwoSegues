1d92d76c7b9f8b001df10e432f19c1aa1218b926
adhoc <adhoc@adhocs-imac.local>
2016-04-15 12:55:11 -0700

    Initial Commit

---
+++ two/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  two
+//
+//  Created by adhoc on 08/07/1437 AH.
+//  Copyright © 1437 AH adhoc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
